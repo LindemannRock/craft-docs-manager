a58403139269a258b24d3b312f35bb738012f6e4
bhlindemann <bh@lindemannrock.com>
2026-05-31 10:20:02 +0400

    fix(settings): correct error message for saving settings

--- src/controllers/SettingsController.php
+++ src/controllers/SettingsController.php
@@ -243,7 +243,7 @@ class SettingsController extends Controller
 
             Craft::$app->getSession()->setNotice(Craft::t('docs-manager', 'Settings saved.'));
         } else {
-            Craft::$app->getSession()->setError(Craft::t('docs-manager', 'Could not save settings'));
+            Craft::$app->getSession()->setError(Craft::t('docs-manager', 'Could not save settings.'));
             return null;
         }
 
